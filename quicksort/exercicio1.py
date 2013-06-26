9caf33b2dfbdb517cd84bf5a0f2ee2805922e7a5
eltonsantos <eltonaxl@hotmail.com>
2013-06-26 02:39:04 -0300

    Commiting readme and file exercicio1.py

---
+++ quicksort/exercicio1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+# Considerando o seguinte vetor:
+#    [25, 48, 37, 12, 57, 86, 33, 92]
+#  
+# 1) Realize a ordenação do vetor em ordem crescente.
+
+
+# Receber a lista
+quick = [25, 48, 37, 12, 57, 86, 33, 92]
+# Escolher o pivo
+
+
+print quick
