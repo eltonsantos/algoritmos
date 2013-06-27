5af62f1cd421747974f310078c78fb2be00c2488
eltonsantos <eltonaxl@hotmail.com>
2013-06-27 02:22:23 -0300

    Commiting exercice1 and exercice2

--- quicksort/exercicio1.py
+++ quicksort/exercicio1.py
@@ -8,9 +8,20 @@
 # 1) Realize a ordenação do vetor em ordem crescente.
 
 
-# Receber a lista
+# Instance a list
 quick = [25, 48, 37, 12, 57, 86, 33, 92]
-# Escolher o pivo
-
-
+# Make a function with a argument
+def quickSort(arr):
+    # Instance a list with a number less that pivot
+    number_less = []
+    # Instance a pivot list
+    pivot_list = []
+    # Instance a list with a number more that pivot
+    number_more = []
+    # If length list less that one return pivot else 
+    if len(arr) <= 1:
+        return arr
+    else:
+    	pass
+    	
 print quick
